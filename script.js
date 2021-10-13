0255e37241f5bcbe92eeeba16638e71be8c9329f
lisa <lisa.marie.lull@gmail.com>
2021-10-13 15:33:18 -0400

    createcards

--- script.js
+++ script.js
@@ -2,18 +2,60 @@
 
 const timer = document.querySelector(".timer");
 const startBtn = document.querySelector(".start-btn");
+const gameBoard = document.querySelector(".gameboard");
 
 let timeLeft = 60;
 let countdownId = 0;
 
 const cardArray = [
-  { type: "pumpkin" },
-  { type: "witch" },
-  { type: "cobweb" },
-  { type: "candy" },
-  { type: "ghost" },
-  { type: "bats" },
+  { type: "pumpkin", img: "assets/pumpkin.png" },
+  { type: "pumpkin", img: "assets/pumpkin.png" },
+  { type: "witch", img: "assets/witch.png" },
+  { type: "witch", img: "assets/witch.png" },
+  { type: "cobweb", img: "assets/spider-web.png" },
+  { type: "cobweb", img: "assets/spider-web.png" },
+  { type: "candy", img: "assets/candy.png" },
+  { type: "candy", img: "assets/candy.png" },
+  { type: "ghost", img: "assets/ghost.png" },
+  { type: "ghost", img: "assets/ghost.png" },
+  { type: "bats", img: "assets/bats.png" },
+  { type: "bats", img: "assets/bats.png" },
 ];
+
+{
+  /* <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+    </div>
+    <div class="flip-card-back">
+          <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+  </div>
+</div> */
+}
+
+const createCards = (array) => {
+  array.forEach((card) => {
+    const flipCard = document.createElement("div");
+    flipCard.classList.add("flip-card");
+    const flipCardInner = document.createElement("div");
+    flipCardInner.classList.add("flip-card-inner");
+    const flipCardFront = document.createElement("div");
+    flipCardFront.classList.add("flip-card-front");
+    const flipCardBack = document.createElement("div");
+    flipCardBack.classList.add("flip-card-back");
+    const backImage = document.createElement("img");
+    backImage.setAttribute("src", card.img);
+    backImage.classList.add("back-image");
+    // might need to attach data attribute to flipcardinner that uses type
+    flipCardBack.append(backImage);
+    flipCardInner.append(flipCardFront, flipCardBack);
+    flipCard.append(flipCardInner);
+    gameBoard.append(flipCard);
+  });
+};
+
+createCards(cardArray);
 
 const shuffle = (array) => {
   let copy = [];
@@ -41,4 +83,10 @@ startBtn.addEventListener("click", (e) => {
   setInterval(countdown, 1000);
 
   //card function here
+});
+
+gameBoard.addEventListener("click", (e) => {
+  if (e.target.classList.contains("flip-card-front")) {
+    e.target.parentNode.classList.add("flip");
+  }
 });
